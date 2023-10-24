ffdbff44e94052634f65cd287aebb1a52f188c2b
carlosrobert0 <carlosroberto15755@gmail.com>
2023-10-23 23:27:18 -0300

    feat: creating web project with nextjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,30 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        roboto: ['var(--font-roboto)'],
+        inter: ['var(--font-inter)'],
+      },
+      colors: {
+        blue_100: '#E8EEF7',
+        blue_300: '#2A5EBC',
+        blue_500: '#1A428A',
+
+        gray_100: '#D5DDE9',
+        gray_300: '#E3E4E7',
+        gray_500: '#707070',
+        gray_700: '#515C6F',
+        gray_900: '#131523',
+
+        green_500: '#2ED9C3',
+
+        yellow_500: '#FFCD35',
+        yellow_700: '#FFBA00',
+
+        red_500: '#E74C3C',
       },
     },
   },
