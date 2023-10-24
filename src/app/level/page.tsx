63ba9ddc0755aeed46be145973b5de5587b01c32
carlosrobert0 <carlosroberto15755@gmail.com>
2023-10-24 18:43:34 -0300

    fix: adjustments on levels page

--- src/app/level/page.tsx
+++ src/app/level/page.tsx
@@ -1,42 +1,61 @@
+'use client'
+import { DialogCreateLevel } from '@/components/Dialog/DialogCreateLevel'
+import { Input } from '@/components/Input'
 import { renderIcon } from '@/utils/renderIcon'
+import { useState } from 'react'
 
 export default function Level() {
+  const [isOpenDialogCreateLevel, setIsOpenDialogCreateLevel] = useState(false)
+
+  function handleOpenDialogCreateLevel() {
+    setIsOpenDialogCreateLevel(true)
+  }
+
+  function handleCloseDialogCreateLevel() {
+    setIsOpenDialogCreateLevel(false)
+  }
+
   return (
-    <main className="w-full h-full bg-gray_100 px-24 flex flex-col items-center justify-start gap-4">
-      <div className="flex flex-col gap-2 w-full">
-        <h1 className="text-2xl text-gray_900 font-medium text-center">
-          Níveis
-        </h1>
-        <div className="flex justify-between gap-4 w-full">
-          <button className="bg-blue_300 rounded-md text-white w-fit p-2">
-            {renderIcon('add')}
-          </button>
-          <input
-            type="text"
-            placeholder="Buscar nível"
-            className="px-4 rounded-md w-96"
-          />
+    <>
+      <main className="w-full h-full bg-gray_100 px-24 flex flex-col items-center justify-start gap-4">
+        <div className="flex flex-col gap-2 w-full">
+          <h1 className="text-2xl text-gray_900 font-medium text-center">
+            Níveis
+          </h1>
+          <div className="flex justify-between gap-4 w-full">
+            <button
+              className="bg-blue_300 rounded-md text-white w-fit p-2"
+              onClick={handleOpenDialogCreateLevel}
+            >
+              {renderIcon('add')}
+            </button>
+            <Input type="text" placeholder="Buscar nível" />
+          </div>
         </div>
-      </div>
-      <table className="w-[920px] border-separate bg-white rounded-lg border-spacing-4">
-        <thead>
-          <tr className="text-blue_300">
-            <th>Nível</th>
-            <th>Desenvolvedores</th>
-            <th>Ação</th>
-          </tr>
-        </thead>
-        <tbody className="text-center">
-          <tr>
-            <td>Pleno</td>
-            <td>Carlos</td>
-            <td className="flex gap-2 justify-center">
-              {renderIcon('edit')}
-              {renderIcon('delete')}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </main>
+        <table className="w-[920px] border-separate bg-white rounded-lg border-spacing-4">
+          <thead>
+            <tr className="text-blue_300">
+              <th>Nível</th>
+              <th>Desenvolvedores</th>
+              <th>Ação</th>
+            </tr>
+          </thead>
+          <tbody className="text-center">
+            <tr>
+              <td>Pleno</td>
+              <td>Carlos</td>
+              <td className="flex gap-2 justify-center">
+                {renderIcon('edit')}
+                {renderIcon('delete')}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
+      <DialogCreateLevel
+        isOpen={isOpenDialogCreateLevel}
+        onCloseDialog={handleCloseDialogCreateLevel}
+      />
+    </>
   )
 }
