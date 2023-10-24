665a44b9a5ba3e98d65345c44629e5fce56e1574
carlosrobert0 <carlosroberto15755@gmail.com>
2023-10-24 18:43:01 -0300

    feat: creating DialogCreateLevel component

---
+++ src/components/Dialog/DialogCreateLevel.tsx
@@ -0,0 +1,49 @@
+import { renderIcon } from '@/utils/renderIcon'
+import * as Dialog from '@radix-ui/react-dialog'
+import { Button } from '../Button'
+import { Input } from '../Input'
+
+interface DialogCreateLevelProps {
+  isOpen: boolean
+  onCloseDialog: () => void
+}
+
+export function DialogCreateLevel({
+  isOpen,
+  onCloseDialog,
+}: DialogCreateLevelProps) {
+  return (
+    <Dialog.Root open={isOpen}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-gray_900/40" />
+        <Dialog.Content className="w-1/3 h-fit bg-white rounded-md p-10 flex flex-col fixed ml-auto top-20 left-1/3 gap-8">
+          <Dialog.Title className="flex">
+            Criar nível
+            <button onClick={onCloseDialog} className="ml-auto text-blue_300">
+              {renderIcon('close')}
+            </button>
+          </Dialog.Title>
+          <form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4">
+              <div className="flex gap-2 items-center text-base justify-between">
+                <label htmlFor="name">Nível:</label>
+                <Input type="text" id="name" placeholder="Nome" />
+              </div>
+            </div>
+            <div className="flex gap-2 justify-end">
+              <Button
+                onClick={onCloseDialog}
+                title="Cancelar"
+                className="bg-white text-blue_300 border-gray_100"
+              />
+              <Button
+                title="Salvar"
+                className="bg-blue_300 text-white border-blue_300"
+              />
+            </div>
+          </form>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
