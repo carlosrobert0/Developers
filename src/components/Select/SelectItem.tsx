8d0f9838c17dc9f56f9c0234626ebde5ff2ccf53
carlosrobert0 <carlosroberto15755@gmail.com>
2023-10-26 07:08:22 -0300

    feat: creating Select component with radixui

--- src/components/Select/SelectItem.tsx
+++ src/components/Select/SelectItem.tsx
@@ -7,11 +7,11 @@ export interface SelectItemProps extends Select.SelectItemProps {
   text: string
 }
 
-export function SelectItem({ text, ...props }: SelectItemProps) {
+export function SelectItem({ text, ...rest }: SelectItemProps) {
   return (
     <Select.Item
       className="flex items-center justify-between gap-2 px-3 py-2.5 outline-none data-[highlighted]:bg-zinc-50"
-      {...props}
+      {...rest}
     >
       <Select.ItemText className="text-black">{text}</Select.ItemText>
       <Select.ItemIndicator>{renderIcon('check')}</Select.ItemIndicator>
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -3,14 +3,14 @@ import { renderIcon } from '@/utils/renderIcon'
 import * as SelectPrimitive from '@radix-ui/react-select'
 import { ReactNode } from 'react'
 
-export interface SelectProps {
+export interface SelectProps extends SelectPrimitive.SelectProps {
   children: ReactNode
   placeholder: string
 }
 
-export function Select({ children, placeholder }: SelectProps) {
+export function Select({ children, placeholder, ...rest }: SelectProps) {
   return (
-    <SelectPrimitive.Root>
+    <SelectPrimitive.Root {...rest}>
       <SelectPrimitive.Trigger className="flex items-center justify-between gap-2 px-3 py-2.5 outline-none">
         <SelectPrimitive.Value
           className="text-black"
@@ -26,7 +26,7 @@ export function Select({ children, placeholder }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="bg-white rounded-md shadow-md outline-none"
+          className="bg-white rounded-md shadow-md outline-none max-h-96 overflow-y-auto"
         >
           <SelectPrimitive.Viewport className="outline-none">
             {children}
