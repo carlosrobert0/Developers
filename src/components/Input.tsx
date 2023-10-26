58ed93e05de64f9d6204e35ee1d4635288d5b0d1
carlosrobert0 <carlosroberto15755@gmail.com>
2023-10-26 07:11:30 -0300

    fix: adjustments in component Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,17 @@
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+import { forwardRef, InputHTMLAttributes } from 'react'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string
 }
 
-export function Input({ className, ...rest }: InputProps) {
-  return (
-    <input
-      {...rest}
-      type="text"
-      className={`px-4 rounded-md w-72 border border-gray_100 focus:border-blue_300 h-10 ${className}}`}
-    />
-  )
-}
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...rest }, ref) => {
+    return (
+      <input
+        {...rest}
+        ref={ref}
+        className={`px-4 rounded-md w-72 border border-gray_100 focus:border-blue_300 h-10 ${className}}`}
+      />
+    )
+  },
+)
